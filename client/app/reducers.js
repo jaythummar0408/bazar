a37a638d473d437d7d5efdb43d2e8132e7eaf80a
jaythummar0408 <jppatel10125@gmail.com>
2023-12-30 11:50:34 +0530

    updated code

--- client/app/reducers.js
+++ client/app/reducers.js
@@ -8,6 +8,7 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { reducer as notifications } from 'react-notification-system-redux';
 
+
 // import reducers
 import applicationReducer from './containers/Application/reducer';
 import homepageReducer from './containers/Homepage/reducer';
